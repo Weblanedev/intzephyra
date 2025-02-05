b9ac63bfbe5b6ffa42a3272cd4f7c26241531696
Bunmi Oye <bunmioye09@gmail.com>
2025-02-05 14:24:20 +0100

    initial commit

---
+++ src/data/brand-data.ts
@@ -0,0 +1,10 @@
+export const brand_data = {
+  name: 'Yoventa',
+  full_name: 'Yoventa Limited ',
+  slogan: 'Compute The Future',
+  description: 'Yoventa is your one-stop-shop for your global computing needs.',
+  phone: '+234-909-636-1340',
+  email: 'operations@yoventaltd.com',
+  address: '',
+  map: '',
+};
