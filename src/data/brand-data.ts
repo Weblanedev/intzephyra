4d109810d09dd2cc02b1111189deca940d00d2ba
Bunmi Oye <bunmioye09@gmail.com>
2025-02-05 14:53:08 +0100

    update brand info

--- src/data/brand-data.ts
+++ src/data/brand-data.ts
@@ -1,10 +1,10 @@
 export const brand_data = {
-  name: 'Yoventa',
-  full_name: 'Yoventa Limited ',
+  name: 'Zephyra',
+  full_name: 'Intercontinental Zephyra Limited',
   slogan: 'Compute The Future',
   description: 'Yoventa is your one-stop-shop for your global computing needs.',
-  phone: '+234-909-636-1340',
-  email: 'operations@yoventaltd.com',
+  phone: '+234-909-636-1197',
+  email: 'accounts@intzephyra.com',
   address: '',
   map: '',
 };
